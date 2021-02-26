ae657885d72c3123f69488510928e9a7a37fe788
JackySuen <77609271+boxiangsuen@users.noreply.github.com>
2021-02-26 11:14:59 +0800

    [2021-2-26 11:14:29] 1、swagger 签入

---
+++ swaggerDemo/swaggerDemo/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace swaggerDemo.Controllers
+{
+    [Route("api/swagger")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        [HttpPost("GetXX")]
+        public String GetXX() {
+            return "Hello World!";
+        }
+
+        [HttpPost("GetNum")]
+        public Int32 GetNum() {
+            return 1;
+        }
+
+        [HttpPost("GetVoid")]
+        public void GetVoid() { 
+        
+        }
+    }
+}
